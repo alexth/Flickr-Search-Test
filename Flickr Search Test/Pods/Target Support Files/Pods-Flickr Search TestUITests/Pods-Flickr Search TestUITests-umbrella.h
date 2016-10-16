af1186b7da5c870aba76730bb73f334a56bc481e
Alex Golub <alexeythumb@gmail.com>
2016-10-16 12:11:36 +0300

    Add Xcode project and cocoapods

---
+++ Flickr Search Test/Pods/Target Support Files/Pods-Flickr Search TestUITests/Pods-Flickr Search TestUITests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_Flickr_Search_TestUITestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Flickr_Search_TestUITestsVersionString[];
+
